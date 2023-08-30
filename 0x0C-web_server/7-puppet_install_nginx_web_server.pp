dc91cb2341ed99f975968a39fa954c7a6c3a27fc
SAMKE-glitch <sammythemwa@gmail.com>
2023-08-30 08:36:23 -0700

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,6 +10,11 @@ exec {'install':
 
 }
 
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell
+}
+
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\t\\treturn 301 https:\/\/dochine.tech\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
