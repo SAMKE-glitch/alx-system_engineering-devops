b5c23443e40e818686708601f72d87a6cf0575e0
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-09 02:48:24 -0700

    modifies my load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ exec {'update':
   ensure => 'present',
 }
 -> file_line { 'http_header':
-  path  => '/etc/nginx.nginx.conf',
+  path  => '/etc/nginx/nginx.conf',
   math  => 'http {',
   line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
