b781867eb2453f1c97e97c8211de19ffb454f893
SAMKE-glitch <sammythemwa@gmail.com>
2023-10-10 21:27:17 +0300

    top ten subredits

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Prints the title for the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit.
+    """
+    if subreddit is Noneor not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        myData = results.get('data').get('children')
+
+        for i in myData:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print('None')
