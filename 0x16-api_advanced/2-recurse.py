0e7c4d0cf4dd35f0c610b0e02ddd5a2c2c5017de
SAMKE-glitch <sammythemwa@gmail.com>
2023-10-10 21:47:42 +0300

    recursive it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Using reddit's API
+"""
+import requests
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    recursive function that queries the Reddit API and returns a
+    list containing the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit,
+    the function should return None.
+    """
+    global after
+    user_agent = {'User-Agent': 'api_advanced_project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
