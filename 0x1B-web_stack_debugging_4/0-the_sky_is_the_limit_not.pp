37f99533d09e57ea034db088dbcb52510dfad86a
SAMKE-glitch <sammythemwa@gmail.com>
2023-10-20 12:29:50 +0300

    puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,13 @@
-class myapp::nginx_optimization {
-
-  # Install Nginx
-  package { 'nginx':
-    ensure => present,
-  }
-
-  # Increase the ULIMIT
-  file { '/etc/security/limits.d/nginx.conf':
-    ensure  => file,
-    content => "nginx hard nofile 4096\nnginx soft nofile 4096",
-  }
-
-  # Enable and start the Nginx service
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => Package['nginx'],
-  }
-}
-
+# This puppet manuscript increases the amount of traffic an Nginx server can handle 
+  
+ # Increase the ULIMIT of the default file 
+ exec { 'fix--for-nginx': 
+   command => 'sed -i "s/15/4096/" /etc/default/nginx', 
+   path    => '/usr/local/bin/:/bin/' 
+ } 
+  
+ # Restart Nginx 
+ -> exec { 'nginx-restart': 
+   command => 'nginx restart', 
+   path    => '/etc/init.d/' 
+ }
